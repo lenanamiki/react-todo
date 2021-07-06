d11e655a6f0d676cc5a40a861b7a9d552b59f87b
Lena Namiki <lena.namiki@gmail.com>
2021-07-06 10:40:48 +0900

    Created App component with placeholder checkbox list

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function App() {
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <div>
+      <input type="checkbox" />
+      <p>Placeholder Text</p>
+      <input type="checkbox" />
+      <p>Placeholder Text</p>
+      <input type="checkbox" />
+      <p>Placeholder Text</p>
+      <input type="checkbox" />
+      <p>Placeholder Text</p>
+    </div>
+  );
+}
+
+export default App;
