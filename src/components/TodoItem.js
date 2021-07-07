d6111d0afd96487c50c52fa138a75d88be3a014d
Lena Namiki <lena.namiki@gmail.com>
2021-07-07 14:24:04 +0900

    Created onClick function

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 function TodoItem(props) {
+  const changeStatus = () => {
+    console.log("Status Changing!");
+  };
+
   return (
     // eslint-disable-next-line react/jsx-filename-extension
     <div className="todo-item">
-      <input type="checkbox" />
+      <input type="checkbox" onChange={changeStatus} />
       <p>{props.item.text}</p>
     </div>
   );
